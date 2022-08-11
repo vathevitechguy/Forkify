78d45eb93ea117eab0a20620531a5d9768cf8de2
Victor Aromose <48617966+VikkyDev05@users.noreply.github.com>
2022-08-11 18:50:49 +0000

    Setting and loading Bookmarks from Local storage

--- assets/js/views/bookmarkView.js
+++ assets/js/views/bookmarkView.js
@@ -7,6 +7,9 @@ class BookmarksView extends View {
   _errorMessage = 'No bookmarks yet. Find a nice recipe and bookmark it :)';
   _message = '';
 
+  addHandlerRender(handler) {
+    window.addEventListener('load', handler);
+  }
   _generateMarkup() {
     const id = window.location.hash.slice(1);
     return this._data
